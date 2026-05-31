04231b4253801af0392a05113b2a70bcfabcd5ca
domecrazynow-cpu <domecrazynow@gmail.com>
2026-05-31 22:45:16 +0700

    update

--- scripts/ngrok-watchdog.bat
+++ scripts/ngrok-watchdog.bat
@@ -1,10 +1,10 @@
-﻿@echo off
-set NGROK=C:\Users\USER\AppData\Local\Microsoft\WinGet\Packages\Ngrok.Ngrok_Microsoft.Winget.Source_8wekyb3d8bbwe\ngrok.exe
+@echo off
+set NGROK=C:\Users\USER\AppData\Roaming\npm\ngrok.cmd
 set DOMAIN=granola-parchment-unsoiled.ngrok-free.dev
 
 :loop
 echo [%TIME%] Starting ngrok tunnel...
-"%NGROK%" http --domain=%DOMAIN% 3000
+"%NGROK%" http --domain=%DOMAIN% 80
 echo [%TIME%] ngrok exited (code %ERRORLEVEL%). Restarting in 5 seconds...
 timeout /t 5 /nobreak >nul
 goto loop
--- start-bot.bat
+++ start-bot.bat
@@ -1,4 +1,3 @@
-﻿@echo off
+@echo off
 cd C:\Users\USER\line-bot
-pm2 resurrect
 start "ngrok-tunnel" /min cmd /c "%~dp0scripts\ngrok-watchdog.bat"
